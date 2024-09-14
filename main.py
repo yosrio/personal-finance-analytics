0f1b33990d041bd0737f3f974b6406665d3a9345
Yos Rio <yosrio.id@gmail.com>
2024-09-14 07:57:36 +0000

    first

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, World First!"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
